1dfeadb5ee52765498ed2025a04ae6bb94500e4d
Alex Kuzmanov <alexkuzmanov5@gmail.com>
2024-08-01 22:39:16 +0300

    minor fix

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react'
 import './Home.css'
 import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
-import { use } from 'chai'
 import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
 import AppDownload from '../../components/AppDownload/AppDownload'
 
